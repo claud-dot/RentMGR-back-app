932328ecdd20f9d39ddbd6ba892f5b7bdf3ef36f
Claud Mja <claudmja2.0@gmail.com>
2023-10-16 14:32:58 +0300

    [debug] remove header

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,6 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", 'true'); 
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
