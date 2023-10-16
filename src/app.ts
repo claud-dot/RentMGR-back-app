693847c2dd0d95c44cf6eb298669b2adf1d90346
Claud Mja <claudmja2.0@gmail.com>
2023-10-16 15:37:05 +0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -35,11 +35,12 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", 'true'); 
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
 app.use(cookieParser());
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(compression());
 app.use(bodyParser.json());
