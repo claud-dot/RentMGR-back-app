7db7af63bf389c1a339b928bf9f4c7e8fde0bd94
Claud Mja <claudmja2.0@gmail.com>
2023-10-16 18:25:20 +0300

    [debug] get token by bearer

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,25 +3,24 @@ import { get , merge } from 'lodash';
 
 import { getUserBySessionToken } from "./../db/users";
 
-export const isAuthentificated = async (req : express.Request , res : express.Response , next : express.NextFunction)=> {
-    const dataSend : any = {}; 
+export const isAuthentificated = async (req : express.Request , res : express.Response , next : express.NextFunction) => {
+    const dataSend = {};
     try {
-        console.log(req.cookies , "Token");
-        const sessionToken = req.cookies['RENT-AUTH'];
+        const sessionToken = req.get('Authorization').split(' ')[1];
+        if (!sessionToken) {
+            throw new Error("Session required. You must first log in!");
+        }
         
-        if(!sessionToken){
-            throw new Error("Session required , You must first login !");
+        const user = await getUserBySessionToken(sessionToken);
+        console.log(sessionToken);
+        if (!user) {
+            throw new Error("Session forbidden. You must first log in!");
         }
 
-        const user = await getUserBySessionToken(sessionToken);
-        if (!user) {
-            throw new Error("Session forbiden ,  You must first login !");
-        }
-        merge(req , { identity : user });
-        next(); 
+        merge(req, { identity: user });
+        next();
     } catch (error) {
         console.log(error.message);
         res.status(400).json({ error: error.message });
     }
-} 
-
+}
